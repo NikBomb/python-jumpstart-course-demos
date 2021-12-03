7dd520176c7bfb37ab00380a954a12c2a9905c73
NikBomb <nicola.bombace@gmail.com>
2021-12-03 17:58:34 +0000

    App 1 and 2

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,7 @@
+print("--------------------------")
+print("HELLO APP")
+print("--------------------------")
+
+print("What is your name?")
+name = input("Enter your name:")
+print("Nice to meet you, " + name)
